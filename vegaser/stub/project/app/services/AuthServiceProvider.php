53a949359963456ea5fb729fbace517967067c85
slawek <slawomir.zytko@gmail.com>
2014-05-16 17:14:26 +0200

    updated stub

---
+++ vegaser/stub/project/app/services/AuthServiceProvider.php
@@ -0,0 +1,36 @@
+    <?php
+/**
+ * This file is part of Vegas package
+ *
+ * @author Slawomir Zytko <slawomir.zytko@gmail.com>
+ * @copyright Amsterdam Standard Sp. Z o.o.
+ * @homepage https://bitbucket.org/amsdard/vegas-phalcon
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Phalcon\DiInterface;
+use Vegas\DI\ServiceProviderInterface;
+use Phalcon\Mvc\Url as UrlResolver;
+use \Vegas\Session\Adapter\Files as SessionAdapter;
+
+class AuthServiceProvider implements ServiceProviderInterface
+{
+    const SERVICE_NAME = 'auth';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register(DiInterface $di)
+    {
+        $di->set(self::SERVICE_NAME, function () use ($di) {
+            $adapter = new \Vegas\Security\Authentication\Adapter\Standard($di->get('userPasswordManager'));
+            $adapter->setSessionStorage($di->get('sessionManager')->createScope('auth'));
+            $auth = new \Vegas\Security\Authentication($adapter);
+
+            return $auth;
+        }, true);
+
+    }
+} 
